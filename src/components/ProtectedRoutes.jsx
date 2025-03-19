04b10e77adafbd5bec33ba6f12db1909187b7447
MariaMendes11 <mcmendes900@gmail.com>
2025-03-19 10:39:53 -0300

    19-03

---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({children}) =>{
+    const isAuthenticated = localStorage.getItem
+    ("authenticated");
+    return isAuthenticated ? children : <Navigate to="/" />;
+
+}
+export default ProtectedRoute;
